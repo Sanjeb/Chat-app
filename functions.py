97321c4f0ae1ba754d58bef13c7369bdd1ca4fdf
Sanjeev <sanjeev.selvam@gmail.com>
2022-10-02 19:48:13 +0530

    made changes to create_group(), created add_participant_to_group()

--- functions.py
+++ functions.py
@@ -18,14 +18,25 @@ def create_user(email, name, password):
         loging.error('Error creating new user')
 
 
-def create_group(groupName, *participantIDs):
+def create_group(groupName, ownerID, participant1, participant2, *participantIDs):
     try:
         cursor.execute(f"INSERT INTO `groups` (`group name`) VALUES ('{groupName}')")
         id = cursor.lastrowid
+        cursor.execute(f"INSERT INTO `group members` (`users_user id`, `groups_group id`) VALUES ('{participant1}', {id})")
+        cursor.execute(f"INSERT INTO `group members` (`users_user id`, `groups_group id`) VALUES ('{participant2}', {id})")
         for x in participantIDs:
             cursor.execute(f"INSERT INTO `group members` (`users_user id`, `groups_group id`) VALUES ('{x}', {id})")
         mydb.commit()   
     except:
         logging.error("Couldn't create group")
-    
-create_group('newgrp')
+
+def add_participant_to_group(groupID, participantID, *participantIDs):
+    try:
+        cursor.execute(f"INSERT INTO `group members` (`users_user id`, `groups_group id`) VALUES ('{participantID}', '{groupID}');")
+        for x in participantIDs:
+            cursor.execute(f"INSERT INTO `group members` (`users_user id`, `groups_group id`) VALUES ('{x}', '{groupID}');")
+        mydb.commit()
+    except:
+        logging.error("Couldn't create group")
+
+add_participant_to_group(1, 1)
