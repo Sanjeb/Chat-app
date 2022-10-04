a2fd22aea6fe09d214c2e76dc3e85d2ead39f2f8
Sanjeev <sanjeev.selvam@gmail.com>
2022-10-04 17:06:53 +0530

    Completed UI for login page

---
+++ UI_login.py
@@ -0,0 +1,48 @@
+import tkinter
+import customtkinter
+import functions_user
+
+#Declaring app variables
+app = customtkinter.CTk()
+app.geometry("10000*10000")
+app.title("LOGIN")
+
+#On login button press function
+def on_press():
+    ret = functions_user.login(entryEmail.get(), entryPassword.get())
+    if ret == 0:
+        labelIncorrect.configure(text="")
+        print("Success")
+    elif ret == 1:
+        labelIncorrect.configure(text="Incorrect email or password. Please try again")
+    else:
+        labelIncorrect.configure(text="Unknown error. Please check your internet connection.")
+
+#Configures app grid rows
+app.rowconfigure((0, 9), weight = 4)
+#Configures app grid columns
+app.columnconfigure((0, 3), minsize = 50)
+app.columnconfigure((1, 2), weight = 1)
+
+#Text labels
+labelWelcome = customtkinter.CTkLabel(master=app, text="Welcome back", text_font=('comic sans', 30))
+labelSignIn = customtkinter.CTkLabel(master=app, text="Sign In to start using chat", text_font=('comic sans', 20))
+labelEmail = customtkinter.CTkLabel(master=app, text="E-mail:\t\t\t", text_font=('comic sans', 10))
+labelPassword = customtkinter.CTkLabel(master=app, text="Password:\t\t", text_font=('comic sans', 10))
+labelIncorrect = customtkinter.CTkLabel(master=app, text="", text_font=('comic sans', 10), text_color='red')
+#Entry boxes
+entryEmail = customtkinter.CTkEntry(master=app, placeholder_text="Enter your email", width=200)
+entryPassword = customtkinter.CTkEntry(master=app, placeholder_text="Enter you password", width=200, show = '\u2022')
+#Buttons
+buttonLogin = customtkinter.CTkButton(master=app, text='LOGIN', width=200, command=on_press)
+
+labelWelcome.grid(row=1, column=1, sticky='w', pady = 0)
+labelSignIn.grid(row=2, column=1, sticky = 'w', pady = 0)
+labelEmail.grid(row=3, column=1, sticky = 'w', pady = (20, 0), padx = (0, 50))
+entryEmail.grid(row=4, column=1, sticky='w')
+labelPassword.grid(row=5, column=1, sticky = 'w', pady = (10, 0), padx = (0, 50))
+entryPassword.grid(row=6, column=1, sticky='w', pady = (0, 20))
+buttonLogin.grid(row=7, column=1, sticky='w')
+labelIncorrect.grid(row=8, column=1, sticky='w')
+
+app.mainloop()
