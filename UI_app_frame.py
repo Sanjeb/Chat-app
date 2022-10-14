2da2311ed336d1349e8672f99bff2d23814f22b7
Sanjeev <sanjeev.selvam@gmail.com>
2022-10-14 16:04:11 +0530

    combined files

--- UI_app_frame.py
+++ UI_app_frame.py
@@ -3,8 +3,42 @@ import customtkinter
 import logging
 import time
 import UI_chat
+import functions_chat
 
 app = customtkinter.CTk()
+
+def chat():
+    global app
+
+    app.columnconfigure(0, minsize = 75)
+    app.columnconfigure(1, minsize = 300)
+    app.columnconfigure(2, weight = 1)
+
+    chats = customtkinter.CTkFrame(master=app, fg_color='red')
+    chat_window = customtkinter.CTkFrame(master=app)
+    profile = customtkinter.CTkFrame(master=app, fg_color='red', width=10)
+    entry = customtkinter.CTkEntry(master=chat_window)
+    messages = customtkinter.CTkFrame(master=chat_window)
+    canvas = tkinter.Canvas(master=messages)
+    scrollbar = customtkinter.CTkScrollbar(master=messages, command=canvas.yview)
+    
+    chats.grid(column = 1, row = 0, sticky = 'nsew')
+    chat_window.grid(column = 2, row = 0, sticky = 'nsew')
+    profile.grid(column = 3, row = 0, sticky = 'nsew')
+    entry.grid(row = 2, padx = 10, pady = 10, sticky = 'ew')
+    messages.grid(row = 1, column=0, sticky = 'nsew')
+    canvas.grid(row = 0, column= 0, sticky='nsew')
+    scrollbar.grid(row = 0, column = 2, sticky = 'nse')
+
+    chat_window.rowconfigure(0, minsize = 75)
+    chat_window.rowconfigure(1, weight = 1)
+    chat_window.columnconfigure(0, weight = 1)
+    messages.columnconfigure(0, weight = 1)
+    messages.rowconfigure(0, weight = 1)
+
+    m = functions_chat.get_dm_messages(1)
+    for x in m:
+        customtkinter.CTkLabel(master=canvas, text = x[1]).pack()
 
 def main():
     global app
@@ -14,6 +48,8 @@ def main():
     app.rowconfigure(0, weight = 1)
     app.rowconfigure(1, minsize = 75)
     
-    UI_chat.main()
+    chat()
 
     app.mainloop()
+
+main()
--- UI_chat.py
+++ UI_chat.py
@@ -4,24 +4,40 @@ import logging
 import UI_app_frame
 import functions_chat
 
-def main():
-    UI_app_frame.app.columnconfigure(0, minsize = 75)
-    UI_app_frame.app.columnconfigure(1, minsize = 300)
-    UI_app_frame.app.columnconfigure(2, weight = 1)
+app = customtkinter.CTk()
 
-    chats = customtkinter.CTkFrame(master=UI_app_frame.app, fg_color='red')
-    chat_window = customtkinter.CTkFrame(master=UI_app_frame.app)
-    profile = customtkinter.CTkFrame(master=UI_app_frame.app, fg_color='red', width=10)
+def main():
+    global app
+
+    app.columnconfigure(0, minsize = 75)
+    app.columnconfigure(1, minsize = 300)
+    app.columnconfigure(2, weight = 1)
+
+    chats = customtkinter.CTkFrame(master=app, fg_color='red')
+    chat_window = customtkinter.CTkFrame(master=app)
+    profile = customtkinter.CTkFrame(master=app, fg_color='red', width=10)
+    entry = customtkinter.CTkEntry(master=chat_window)
+    messages = customtkinter.CTkFrame(master=chat_window)
     scrollbar = customtkinter.CTkScrollbar(master=chat_window)
-    #scrollbar.pack(side=RIGHT, fill=Y)
     
     chats.grid(column = 1, row = 0, sticky = 'nsew')
     chat_window.grid(column = 2, row = 0, sticky = 'nsew')
     profile.grid(column = 3, row = 0, sticky = 'nsew')
+    entry.grid(row = 2, padx = 10, pady = 10, sticky = 'ew')
+    messages.grid(row = 1, column=0, padx = 10, sticky = 'ews')
+    scrollbar.grid(row = 1, column = 2, sticky = 'nse')
 
+    chat_window.rowconfigure(0, minsize = 75)
+    chat_window.rowconfigure(1, weight = 1)
+    chat_window.columnconfigure(0, weight = 1)
+    messages.columnconfigure(0, weight = 1)
+   
     def display_dm_messages(dmID):
         messages = functions_chat.get_dm_messages(dmID)
         for x in messages:
-            customtkinter.CTkLabel(master=chat_window, text=x[1]).grid(column=1, sticky = 'w')
-    display_dm_messages(1)
-    display_dm_messages(1)
+            message = customtkinter.CTkLabel(master = messages, text='hi')
+            message.grid(column = 1)
+        
+    
+    for x in range(25):
+        display_dm_messages(1)
